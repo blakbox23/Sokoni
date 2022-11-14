2217cf6eefbc3ed1999b0fc4c9b0b04bd8f0b348
blakbox23 <boudii30@gmail.com>
2022-11-14 22:22:17 +0300

    Make the footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,40 +1,42 @@
+import { Col, Row } from "react-bootstrap";
+
 export function Footer() {
   return (
     <>
-      <div
+      <Row
         style={{ backgroundColor: "#10140c" }}
-        className="d-flex p-4 justify-content-around"
+        className="p-5 justify-content-around"
       >
-        <div>
+        <Col>
           <p className="fw-bold text-light">Let's do it together</p>
           <p className="text-white-50">Careers</p>
           <p className="text-white-50">Soko yetu Partners</p>
           <p className="text-white-50">Soko yet business</p>
           <p className="text-white-50">More</p>
-        </div>
+        </Col>
 
-        <div>
+        <Col>
           <p className="fw-bold text-light">Link our interest</p>
           <p className="text-white-50">About us</p>
           <p className="text-white-50">FAQ</p>
           <p className="text-white-50">Products</p>
           <p className="text-white-50">Contact us</p>
-        </div>
+        </Col>
 
-        <div>
+        <Col>
           <p className="fw-bold text-light">Follow us</p>
           <p className="text-white-50">Instagram</p>
           <p className="text-white-50">Twitter</p>
           <p className="text-white-50">Facebook</p>
           <p className="text-white-50">Linked</p>
-        </div>
+        </Col>
 
-        <div className="mt-5">
+        <Col className="mt-4">
           <p className="text-white-50">TERMS AND CONDITIONS</p>
           <p className="text-white-50">PRIVACY POLICY</p>
           <p className="text-white-50">COOKIES SETTINGS</p>
-        </div>
-      </div>
+        </Col>
+      </Row>
       <div
         className="d-flex justify-content-center align-items-center text-white bg-dark"
         style={{ height: "3rem", fontSize: ".9rem", fontWeight: "300" }}
